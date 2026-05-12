703fa4a4e858b9073de7b4c80e016cb71d92afb8
aboyasser222 <156500193+aboyasser222@users.noreply.github.com>
2026-05-12 16:46:41 +0300

    Update app.py

--- codeCar/app.py
+++ codeCar/app.py
@@ -7,13 +7,13 @@ import requests
 NGROK_URL = "https://autistic-revenge-unending.ngrok-free.dev"
 
 st.set_page_config(page_title="Autonomous Navigation System", layout="wide")
-st.title("🛥️ Water Hyacinth & Boat Detection System")
+st.title("🛥️ Boat & Water Hyacinth Detection System")
 
 @st.cache_resource
 def load_model():
     model = YOLO("best (5).pt")
-    model.names[0] = "water_hyacinth"
-    model.names[1] = "boat"
+    model.names[0] = "boat"
+    model.names[1] = "water_hyacinth"
     return model
 
 model = load_model()
@@ -25,12 +25,16 @@ if camera_input is not None:
     img_array = np.array(image)
     
     results = model(img_array, conf=0.77)[0]
+    
+    results.names[0] = "boat"
+    results.names[1] = "water_hyacinth"
+    
     st.image(results.plot(), caption='Analysis Results', use_column_width=True)
     
     detections = results.boxes.data.tolist()
     
-    hyacinth_detected = any(int(box[5]) == 0 for box in detections)
-    boat_detected = any(int(box[5]) == 1 for box in detections)
+    boat_detected = any(int(box[5]) == 0 for box in detections)
+    hyacinth_detected = any(int(box[5]) == 1 for box in detections)
     
     if boat_detected:
         st.error("🚨 Boat Detected! Reversing and changing course...")
