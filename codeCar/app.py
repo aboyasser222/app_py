61cab063d48d6229b9b98e843c19881b0583af46
aboyasser222 <156500193+aboyasser222@users.noreply.github.com>
2026-05-13 23:01:32 +0300

    Update app.py

--- codeCar/app.py
+++ codeCar/app.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 from PIL import Image
 import requests
 
-NGROK_URL = "http://localhost:5000"
+NGROK_URL = "https://autistic-revenge-unending.ngrok-free.dev"
 
 st.set_page_config(page_title="Autonomous Navigation System", layout="wide")
 st.title("🛥️ Detection System: Boat, Rock & Water Hyacinth")
